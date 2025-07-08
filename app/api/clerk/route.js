98db9a0396bd099eaa815d5111d6a304a419372c
Lnvdien <lnvdien@gmail.com>
2025-07-08 14:21:37 +0700

    test

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -4,11 +4,46 @@ import User from "@/models/User";
 import { headers } from "next/headers";
 import { NextRequest } from "next/server";
 
-export async function GET() {
-  try {
+export async function POST(req) {
+    const wh = new Webhook(process.env.SIGNING_SECRET);
+    const headerPayload = headers();
+    const svixHeaders = {
+        "svix-id": headerPayload.get("svix-id"),
+        "svix-timestamp": headerPayload.get("svix-timestamp"),
+        "svix-signature": headerPayload.get("svix-signature"),
+    };
+    //get the payload and verify it
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
+    const { data, type } = wh.verify(body, svixHeaders);
+    //prepare the user data to be saved in the database
+    const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+    };
+    //connect to the database
     await connectDB();
-    return new Response("✅ Connected to DB", { status: 200 });
-  } catch {
-    return new Response("❌ Failed to connect to DB", { status: 500 });
-  }
+    
+    switch (type) {
+        case "user.created":
+            //create a new user
+            await User.create(userData);
+            break;
+        case "user.updated":
+            //update the user
+            await User.findByIdAndUpdate(data.id, userData, { new: true });
+            break;
+        case "user.deleted":
+            //delete the user
+            await User.findByIdAndDelete(data.id);
+            break;
+        default:
+            console.log("Unhandled event type:", type);
+            break;
+    }
+    return NextRequest.json({
+        message: "Event received"
+    });
 }
